34b9945bba3de71fcec3e388a9556414767e2be8
Pradheep Rajendirane <pas495@gmail.com>
2022-04-04 16:51:29 +0200

    handle outgoing conversation

--- routes/frontline/index.ts
+++ routes/frontline/index.ts
@@ -1,18 +1,15 @@
 import { Express } from 'express';
 import crmCallbackHandler from './crm.js';
+import outgoingConversationCallbackHandler from './outgoing-conversation.js';
 import routingCallbackHandler from './routing.js';
 import conversationsCallbackHandler from './twilio-conversations.js';
 import { incomingVoiceActionHandler, incomingVoiceCallbackHandler } from './voice.js';
-
-//const routingCallbackHandler = require('./routing');
-//const outgoingConversationCallbackHandler = require('./outgoing-conversation');
-//const templatesCallbackHandler = require('./templates');
 
 export default (router: Express) => {
 
   router.post("/frontline/callbacks/conversations", conversationsCallbackHandler);
   router.post("/frontline/callbacks/routing", routingCallbackHandler);
-  //router.post("/callbacks/outgoing-conversation", outgoingConversationCallbackHandler);
+  router.post("/frontline/callbacks/outgoing-conversation", outgoingConversationCallbackHandler);
   router.post("/frontline/callbacks/crm", crmCallbackHandler);
   //router.post("/callbacks/templates", templatesCallbackHandler);
   router.post("/frontline/callbacks/voiceIncoming", incomingVoiceCallbackHandler);
---
+++ routes/frontline/outgoing-conversation.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+
+const outgoingConversationCallbackHandler = (req: Request, res: Response) => {
+  console.log('outgoingConversationCallbackHandler');
+
+  const location = req.body.Location;
+
+  // Location helps to determine which action to perform.
+  switch (location) {
+    case 'GetProxyAddress': {
+      handleGetProxyAddress(req, res);
+      return;
+    }
+
+    default: {
+      console.log('Unknown location: ', location);
+      res.sendStatus(422);
+    }
+  }
+};
+
+const handleGetProxyAddress = (req: Request, res: Response) => {
+  console.log('Getting Proxy Address');
+
+  const body = req.body;
+  const workerIdentity = body.Worker;
+  const customerId = body.CustomerId;
+  const channelName = body.ChannelType;
+  const channelAddress = body.ChannelValue;
+
+  const proxyAddress = getCustomerProxyAddress(channelName);
+
+  // In order to start a new conversation ConversationsApp need a proxy address
+  // otherwise the app doesn't know from which number send a message to a customer
+  if (proxyAddress) {
+    res.status(200).send({ proxy_address: proxyAddress });
+    console.log("Got proxy address!");
+    return;
+  }
+
+  console.log("Proxy address not found");
+  res.sendStatus(403);
+};
+
+const getCustomerProxyAddress = (channelName: string) => {
+  if (channelName === 'whatsapp') {
+    return process.env.FRONTLINE_NUMBER;
+  } else {
+    return process.env.FRONTLINE_NUMBER;
+  }
+};
+
+export default outgoingConversationCallbackHandler;
