29334be030b3d0b11e0f996c2c0b33f357819a69
Pradheep Rajendirane <pas495@gmail.com>
2022-04-05 15:48:28 +0200

    Update templates.ts

--- routes/frontline/templates.ts
+++ routes/frontline/templates.ts
@@ -37,19 +37,13 @@ const handleGetTemplatesByCustomerIdCallback = async (req : Request, res : Respo
     const openersCategory = {
         display_name: 'Démarrer une conversation', // Category name
         templates: [
-            { content: compileTemplate(OPENER_NEXT_STEPS, customerDetails) }, // Compiled template
-            { content: compileTemplate(OPENER_NEW_PRODUCT, customerDetails) },
-            { content: compileTemplate(OPENER_ON_MY_WAY, customerDetails), whatsAppApproved: true },
+            { content: compileTemplate(OPENER_NEXT_STEPS, customerDetails) }
         ]
     };
     const repliesCategory = {
         display_name: 'Répondre à un message',
         templates: [
-            { content: compileTemplate(REPLY_SENT, customerDetails) },
-            { content: compileTemplate(REPLY_RATES, customerDetails) },
-            { content: compileTemplate(REPLY_OMW, customerDetails) },
-            { content: compileTemplate(REPLY_OPTIONS, customerDetails) },
-            { content: compileTemplate(REPLY_ASK_DOCUMENTS, customerDetails) },
+            { content: compileTemplate(REPLY_SENT, customerDetails) }
         ]
     };
     const closingCategory = {
@@ -70,15 +64,15 @@ const compileTemplate = (template : string, customer : IFrontlineCustomer) => {
 };
 
 const OPENER_NEXT_STEPS = 'Bonjour {{Name}} nous avons traité vos documents, vous pouvez me contacter ici. {{Author}}.';
-const OPENER_NEW_PRODUCT = 'Hello {{Name}} we have a new product out which may be of interest to your business. Drop me a message. {{Author}}.';
-const OPENER_ON_MY_WAY ='Just to confirm I am on my way to your office. {{Name}}.';
+/*const OPENER_NEW_PRODUCT = 'Hello {{Name}} we have a new product out which may be of interest to your business. Drop me a message. {{Author}}.';
+const OPENER_ON_MY_WAY ='Just to confirm I am on my way to your office. {{Name}}.';*/
 
-const REPLY_SENT = 'This has now been sent. {{Author}}.';
-const REPLY_RATES = 'Our rates for any loan are 20% or 30% over $30,000. You can read more at https://example.com. {{Author}}.';
+const REPLY_SENT = 'Toutes les réductions indiquées sont appliquées après signature. {{Author}}.';
+/*const REPLY_RATES = 'Our rates for any loan are 20% or 30% over $30,000. You can read more at https://example.com. {{Author}}.';
 const REPLY_OMW = 'Just to confirm I am on my way to your office. {{Author}}.';
 const REPLY_OPTIONS = 'Would you like me to go over some options with you {{Name}}? {{Author}}.';
-const REPLY_ASK_DOCUMENTS = 'We have a secure drop box for documents. Can you attach and upload them here: https://example.com. {{Author}}';
+const REPLY_ASK_DOCUMENTS = 'We have a secure drop box for documents. Can you attach and upload them here: https://example.com. {{Author}}';*/
 
-const CLOSING_ASK_REVIEW = 'Happy to help, {{Name}}. If you have a moment could you leave a review about our interaction at this link: https://example.com. {{Author}}.';
+const CLOSING_ASK_REVIEW = 'Je vous remercie de m\'avoir contacté, {{Name}}. Si vous avez d\'autres questions, n\'hésitez à me recontacter sur ce même numéro. {{Author}}.';
 
 export default templatesCallbackHandler;
