1c6fceae284f9466dcae828c69f19f7545a93597
Pradheep Rajendirane <pas495@gmail.com>
2022-04-05 12:39:00 +0200

    remove hardcoded welcome msg

--- routes/frontline/voice.ts
+++ routes/frontline/voice.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 
 export const incomingVoiceCallbackHandler = async (req : Request, res : Response) => {
 
-  const responseBody = `<?xml version="1.0" encoding="UTF-8"?><Response><Say voice="Polly.Lea" language="fr-FR" ><prosody rate="medium">Bonjour bienvenue chez Mutuelle d'assurances du corps de santé français, je vous mets en relation avec votre conseiller.</prosody></Say><Connect action="https://${req.hostname}/frontline/callback/voiceAction"><Conversation serviceInstanceSid="ISa442c5ab66fc4935b3270d3b04c2f7bf" inboundTimeout="20"/></Connect></Response>`;
+  const responseBody = `<?xml version="1.0" encoding="UTF-8"?><Response><Connect action="https://${req.hostname}/frontline/callback/voiceAction"><Conversation serviceInstanceSid="ISa442c5ab66fc4935b3270d3b04c2f7bf" inboundTimeout="20"/></Connect></Response>`;
 
   res.setHeader('Content-Type', 'text/xml').status(200).send(responseBody);
 
